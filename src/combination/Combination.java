f3225902b15b97ef4953377492032d15c096883a
postlude <seungdeokhan@naver.com>
2018-04-13 00:59:22 +0900

    Combination 추가

---
+++ src/combination/Combination.java
@@ -0,0 +1,23 @@
+package combination;
+
+public class Combination {
+	public static void main(String[] args) {
+		Combination combination = new Combination();
+		
+		System.out.println(combination.calcCombination(15, 9));
+	}
+	
+	/**
+	 * nCr 계산하는 메소드
+	 * @param n
+	 * @param r
+	 * @return nCr 계산 결과 값 
+	 */
+	public int calcCombination(int n, int r) {
+		if(n==r || r==0) {
+			return 1;
+		} else {
+			return calcCombination(n-1, r-1) + calcCombination(n-1, r);
+		}
+	}
+}
