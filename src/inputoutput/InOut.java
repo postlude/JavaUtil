aba73833a026ddd3c2dd6c4e85358f1f83b5c07e
postlude <seungdeokhan@naver.com>
2018-05-02 01:53:55 +0900

    InOut class 내용 추가

--- src/inputoutput/InOut.java
+++ src/inputoutput/InOut.java
@@ -54,7 +54,13 @@ public class InOut {
 			BufferedWriter bw2 = new BufferedWriter(new OutputStreamWriter(System.out));
 			){
 			String input = br2.readLine();
-			bw2.write(input);
+			int input2 = Integer.parseInt(br2.readLine());
+			
+			bw2.write(input + "\n");
+			
+			// string 형태로 출력해야 제대로된 숫자값이 나옴
+			// parameter가 int인 write()를 호출하면 입력한 숫자를 아스키코드로 인식해서 출력
+			bw2.write(input2 + "\n");
 		}catch(IOException ioe) {
 			ioe.printStackTrace();
 		}
